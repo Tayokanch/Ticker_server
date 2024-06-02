da39b4e89a4a0d9d646d53822c88138432e0f611
Tayokanch <tayoquadri11@gmail.com>
2024-06-02 22:14:12 +0100

    new port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import {app} from './server.js'
 
-const port = process.env.PORT 
+const port = process.env.PORT || 3318
 app.listen(port, () => {
     console.log(`\n Server is running on http://localhost:${port}\n`);
   });
